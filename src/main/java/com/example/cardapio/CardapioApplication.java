d84bc9fdc5616c7f3c5a891099bbf48867c9b7a2
igortcruz <igortorres213@gmail.com>
2024-10-31 19:52:37 -0300

    build:
    initial commit cardapio

---
+++ src/main/java/com/example/cardapio/CardapioApplication.java
@@ -0,0 +1,13 @@
+package com.example.cardapio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication //Anotação que indica que essa classe é a Aplicação Spring Boot, o começo de tudo.
+public class CardapioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardapioApplication.class, args);
+	}
+
+}
